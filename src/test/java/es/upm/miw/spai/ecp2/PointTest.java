57fee32971f97cf731a8f90e2b3435fc4b6d1e05
guille209 <gguillemiranda@gmail.com>
2014-10-02 18:39:14 +0200

    Corregido bug testDistancia entre puntos

--- src/test/java/es/upm/miw/spai/ecp2/PointTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/PointTest.java
@@ -50,6 +50,6 @@ public class PointTest {
 
     @Test
     public void testDistancia(){
-    	assertEquals(4.0,pt.distancia(new Point(2,1)),10e-3);
+    	assertEquals(2.0,pt.distancia(new Point(2,1)),10e-3);
     }
 }
